0957307994398730193a753b9a3c932913a21309
Shashank01729 <117083031+Shashank01729@users.noreply.github.com>
2024-07-16 14:30:07 +0530

    Randomising Targets, Destroying Targets

--- Assets/Scripts/GamePlayManager.cs
+++ Assets/Scripts/GamePlayManager.cs
@@ -8,11 +8,14 @@ public class GamePlayManager : MonoBehaviour
 
     private void Start()
     {
-        SpawnTarget();
+        InvokeRepeating("SpawnTarget",1f,1f);
     }
     void SpawnTarget() 
     {
-        Instantiate(targetPrefab);
+        float xPos = Random.Range(-7.1f,7.1f);
+        float yPos = Random.Range(-3.1f,3.1f);
+        Vector3 randomTargetPos = new Vector3(xPos, yPos, 0);
+        Instantiate(targetPrefab,randomTargetPos,Quaternion.identity); // Don't want rotation
     }
 
 }
